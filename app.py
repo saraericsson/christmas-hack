8b1d212b2383f97bfcc615ce9e788ff4dc024828
Sara Ericsson <saraerics88@gmail.com>
2019-12-23 22:17:44 +0100

    app.py

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ with app.test_request_context():
     print(url_for('index'))
     print(url_for('login'))
     print(url_for('login', next='/'))
-    print(url_for('profile', username='John Doe'))
+    print(url_for('profile', username='Sara Ericsson'))
