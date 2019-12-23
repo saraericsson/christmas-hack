658dc4bbd1c43efe64ccbf05d726e6522c6397dc
Sara Ericsson <saraerics88@gmail.com>
2019-12-23 22:03:54 +0100

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+def index():
+    return 'Christmas Hack, YOLO'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
